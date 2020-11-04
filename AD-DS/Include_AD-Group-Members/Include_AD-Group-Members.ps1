905358051715da3636897bdca5c15921dfb2ad2a
Felipe Cardoso <50784394+felipescardoso@users.noreply.github.com>
2020-11-04 11:26:57 -0300

    Create Include_AD-Group-Members.ps1

---
+++ AD-DS/Include_AD-Group-Members/Include_AD-Group-Members.ps1
@@ -0,0 +1,12 @@
+﻿# ADD USER TO AD GROUP+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+$GRUP = "GROUP'S_NAME"
+
+$UserAcounts = "USER1","USER2","USER3"
+
+foreach ($UserAcount in $UserAcounts) {
+    $UserAcount = Get-ADUser $UserAcount -Properties *
+    Add-ADGroupMember $GRUP -Members $UserAcount | Out-Null
+    write-host ""
+    write-host  $UserAcount.CN " - " $GRUP
+}
