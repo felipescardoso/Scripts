6583e2ad915382a49de08dfa7bb374a2d1e301ac
Felipe Cardoso <50784394+felipescardoso@users.noreply.github.com>
2021-04-08 16:28:54 -0300

    Upload

---
+++ M365/SP-Upload_file.ps1
@@ -0,0 +1,15 @@
+﻿Install-Module -Name PnP.PowerShell -AllowPrerelease
+
+#Config Variables
+$SiteURL = "https://familiacardoso.sharepoint.com/sites/ORG"
+$SourceFilePath ="C:\@TI\Teste_SP.xlsx"
+$DestinationPath = "/Documents" #Relative Path of the Library
+  
+#Get Credentials to connect
+$Cred = Get-Credential
+  
+#Connect to PNP Online
+Connect-PnPOnline -Url $SiteURL -Credentials $Cred
+      
+#powershell pnp to upload file to sharepoint online
+Add-PnPFile -Path $SourceFilePath -Folder $DestinationPath
---
+++ SCCM/Troubleshooting-Inventory/Full-Hardware_Inventory.ps1
@@ -0,0 +1,15 @@
+﻿<#Descrição: 
+1 - Remove o objeto WMI do hardware inventory;
+2 - Força o inventário de hardware Full do equipamento.
+
+#>
+
+$DelInvSUP = Get-WmiObject -Class "InventoryActionStatus" -ComputerName .  -Namespace "ROOT\ccm\InvAgt"  
+$DelInvSUP | ForEach-Object  {
+    If ($_.InventoryActionID -EQ "{00000000-0000-0000-0000-000000000001}"){
+        $_ | Remove-WmiObject
+    }
+}
+
+Start-Sleep -Seconds 4
+Invoke-WMIMethod -ComputerName . -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule "{00000000-0000-0000-0000-000000000001}"
